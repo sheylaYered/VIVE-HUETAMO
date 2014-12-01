60cd6be11ac98bc27e66fe4f76f5323aeef7c380
lenin <francisco5.fp@gmail.com>
2014-12-01 17:16:03 -0600

    puse el recordatoria de la fecha y descripcion de unas comidas

---
+++ Muere-Huetamo/public_html/fecha.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//scrip externo llamado fecha.js
+var hoy=new Date();
+dia=hoy.getDate();
+mes=hoy.getMonth();
+anyo=hoy.getFullYear();
+
+alert("Te recordamos que Hoy es  " + dia +" del "+ mes+" de "+anyo);
