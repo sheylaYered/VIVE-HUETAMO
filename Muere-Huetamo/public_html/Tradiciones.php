94304139c70524198311ab8136e367cb7a84eed6
joanaita <joana_princess04@hotmail.com>
2014-12-07 23:05:19 -0600

    contacto y tradiciones

--- Muere-Huetamo/public_html/Tradiciones.php
+++ Muere-Huetamo/public_html/Tradiciones.php
@@ -9,9 +9,11 @@
         
 <header>
 </header>
-
+<center><section id="lineatitulo">
 
 <div id="titulo">TRADICIONES </div>
+</section>
+</center>
 <section>
         <div id="caja1"> <h1 align="center" >TRES VIERNES</h1>
 <h2></h2>
